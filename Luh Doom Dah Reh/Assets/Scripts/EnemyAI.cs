beb6c1f556def2b094c89fa224feacb39c582ade
DoomSdaY960325 <balram960325@gmail.com>
2018-08-11 15:51:21 +0800

    minor changes

---
+++ Luh Doom Dah Reh/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour {
+    public static int numEnemies = 0;
+
+    public float hp = 100;
+
+    [SerializeField] float hpLowPercent = 25;
+    [SerializeField] private float stationDmg = 25;
+    [SerializeField] private float flyDmg = 10;
+    [SerializeField] private Material hpLowMat;
+
+    private bool hpLowBool = false;
+
+    private void Update() {
+        HpCheck();
+    }
+
+    private void HpCheck() {
+        if (hpLowBool) {
+            return;
+        }
+
+        if (hp < (hp / hpLowPercent * 100)) {
+            Renderer rend = GetComponent<Renderer>();
+            if (rend != null) {
+                rend.material = hpLowMat;
+            }
+
+            hpLowBool = true;
+        }
+    }
+}
